47e2bea28cb19299f3d896acbd93e93f817b1ea9
ivenawaromi <136889191+ivenawaromi@users.noreply.github.com>
2024-04-02 20:31:12 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//HomeController
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+//ProfileController
+Route::get('profile', ProfileController::class)->name('profile');
+
+//Employee List
+Route::resource('employees', EmployeeController::class);
